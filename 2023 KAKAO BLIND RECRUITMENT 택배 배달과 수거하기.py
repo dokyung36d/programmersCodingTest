c94509df2198b6d75214a03575826c32173b0d85
dokyu <dokyung36@naver.com>
2024-08-17 12:34:59 +0900

    delivery problem solved, but need refactoring

--- 2023 KAKAO BLIND RECRUITMENT 택배 배달과 수거하기.py
+++ 2023 KAKAO BLIND RECRUITMENT 택배 배달과 수거하기.py
@@ -6,6 +6,7 @@
 ##[0, 2, 0, 1, 0, 2, 0]
 
 import sys
+import copy
 sys.setrecursionlimit(10**7)
 
 def solution(cap, n, deliveries, pickups):
@@ -13,23 +14,26 @@ def solution(cap, n, deliveries, pickups):
 
     return result
 
-sum_list = []
 
 def get_result_after_round_trip(go_list, back_list, go_index, back_index, cap, n, sum_distance):
-    global sum_list
-    if go_index == -2 and back_index == -2:
+    if go_index == -1 and back_index == -1:
         return sum_distance
     
     sum_go = 0
     sum_back = 0
     
-    sum_distance += (2 * max([go_index + 1, back_index + 1]))
-    # sum_list.append(2 * max([go_index + 1, back_index + 1]))
+    flag = 0
     
-    while sum_go < cap and go_index >= -1:
+    if go_list[go_index] == 0 and back_list[back_index] == 0:
+        go_index = get_prev_index_which_not_zero(go_list, go_index)
+        back_index = get_prev_index_which_not_zero(back_list, back_index)
+
+    sum_distance += (2 * max([go_index + 1, back_index + 1]))
+        
+    while sum_go < cap and go_index >= 0:
         if (sum_go + go_list[go_index]) < cap:
             sum_go += go_list[go_index]
-            go_index -= 1
+            go_index = get_prev_index_which_not_zero(go_list, go_index)
             
             continue
             
@@ -44,12 +48,15 @@ def get_result_after_round_trip(go_list, back_list, go_index, back_index, cap, n
             go_list[go_index] -= (cap - sum_go)
             sum_go = cap
             
+            if go_list[go_index] // cap > 2:
+                flag = 1
+            
             break
             
-    while sum_back < cap and back_index >= -1:
+    while sum_back < cap and back_index >= 0:
         if sum_back + back_list[back_index] < cap:
             sum_back += back_list[back_index]
-            back_index -= 1
+            back_index = get_prev_index_which_not_zero(back_list, back_index)
             
             continue
             
@@ -63,13 +70,32 @@ def get_result_after_round_trip(go_list, back_list, go_index, back_index, cap, n
             back_list[back_index] -= (cap - sum_back)
             sum_back = cap
             
+            if back_list[back_index] // cap > 2 and flag == 1:
+                go_list, back_list, go_index, back_index, sum_distance = process_hot_place(go_list, back_list, go_index, back_index, cap, sum_distance)
+                
+            
             break
             
     return get_result_after_round_trip(go_list, back_list, go_index, back_index, cap, n, sum_distance)
 
+def process_hot_place(list1, list2, index1, index2, cap, sum_distance):
+    num_iteration = min([list1[index1] // cap, list2[index2] // cap])
+    sum_distance += num_iteration * (2 * max([index1 + 1, index2 + 1]))
+
+    list1[index1] -= cap * num_iteration
+    list2[index2] -= cap * num_iteration
+    
+    if list1[index1] == 0:
+        index1 = get_prev_index_which_not_zero(list1, index1)
+    if list2[index2] == 0:
+        index2 = get_prev_index_which_not_zero(list2, index2)
+        
+    return (list1, list2, index1, index2, sum_distance)
+
 def get_prev_index_which_not_zero(list1 : list, index):
     if index == 0:
-        return -2
+        return -1
+
     index -= 1
     
     while index >= 0:
@@ -77,5 +103,5 @@ def get_prev_index_which_not_zero(list1 : list, index):
             return index
         index -= 1
         
-    return -2
+    return -1
     
