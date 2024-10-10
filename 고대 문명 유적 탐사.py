dc7100e1f8ada4d91be17baa79ecdecd1f155f98
dokyu <dokyung36@naver.com>
2024-10-10 11:45:54 +0900

    ancient base finsih

--- 고대 문명 유적 탐사.py
+++ 고대 문명 유적 탐사.py
@@ -1,11 +1,70 @@
 ##총 9 * 3 경우 가능
 
 from collections import deque
+import sys
+import copy
+
+K,M = map(int, sys.stdin.readline().split())
+
+matrix = []
+for _ in range(5):
+    new_list = list(map(int, sys.stdin.readline().split()))
+    matrix.append(new_list)
+
+replace_list = list(map(int, sys.stdin.readline().split()))
+
+def rotate_all_case(matrix):
+    best_stadard = [360, (6, 6)]
+    best_score = 0
+    best_result = []
+
+    rotate_degree_list = [90, 180, 270]
+    for i in range(1, 4):
+        for j in range(1, 4):
+            for rotate_degree in rotate_degree_list:
+                rotated_matrix = rotate(matrix, rotate_degree, (i, j))
+                result = bfs(rotated_matrix)
+                score = len(result)
+                if score > best_score:
+                    best_score = score
+                    best_result = result
+                    best_stadard = [rotate_degree, (i, j)]
+
+                elif score == best_score:
+                    standard = [rotate_degree, (i, j)]
+                    if check_standard(best_stadard, standard):
+                        best_stadard = standard
+                        best_result = result
+
+    return best_result
+
+def check_standard(standard0, standard1):
+    if standard0[0] < standard1[0]:
+        return 0
+
+    if standard0[0] > standard1[0]:
+        return 1
 
 
-def rotate(matrix, degree):
-    return_matrix = [[0 for _ in range(3)] for _ in range(3)]
-    center = (1, 1)
+    if standard0[1][1] < standard1[1][1]:
+        return 0
+
+    if standard0[1][1] > standard1[1][1]:
+        return 1
+
+
+    if standard0[1][0] < standard1[1][0]:
+        return 0
+
+    if standard0[1][0] > standard1[1][0]:
+        return 1
+
+
+
+
+
+def rotate(matrix, degree, center):
+    return_matrix = copy.deepcopy(matrix)
     num_rotate = degree // 90
 
     edge_list = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
@@ -30,6 +89,8 @@ def rotate(matrix, degree):
     return return_matrix
 
 def bfs(matrix):
+    # score = 0
+    return_list = []
     visited = []
 
     for i in range(5):
@@ -37,11 +98,20 @@ def bfs(matrix):
             if (i, j) in visited:
                 continue
 
+            result = search((i, j), matrix)
+            visited.extend(result)
+
+            if len(result) >= 3:
+                return_list.extend(result)
+                # score += len(result)
+
+    return return_list
+
 def search(start_index, matrix):
     value = matrix[start_index[0]][start_index[1]]
     delta_list = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-    return_list = []
+    return_list = [start_index]
 
     d = deque([start_index])
     while d:
@@ -65,14 +135,34 @@ def search(start_index, matrix):
     return return_list
 
 def check_index(index):
-    if index[0] < 0 or index[0] > 5 or index[1] < 0 or index[1] > 5:
+    if index[0] < 0 or index[0] >= 5 or index[1] < 0 or index[1] >= 5:
         return False
     return True
 
+def sort(index_list):
+    index_list = [(index[1], 9 - index[0]) for index in index_list]
+    index_list.sort()
+    index_list = [(9 - index[1], index[0]) for index in index_list]
+
+    return index_list
+
 
 if __name__ == "__main__":
-    matrix = [[7,6,7], [7,1,5], [6,3,2]]
-    rotated_matrix = rotate(matrix, 90)
-    result = search((0, 1), rotated_matrix)
+    total_score = 0
+
+    # while True:
+    #     # #matrix = [[7,6,7], [7,1,5], [6,3,2]]
+    #     # rotated_matrix = rotate(matrix, 90, (2, 2))
+    #     # result = bfs(rotated_matrix)
+
+    result = rotate_all_case(matrix)
+    print(result)
+    total_score += len(result)
+
+    # result = search((0, 1), rotated_matrix)
+    result = sort(result)
+    for i in range(len(result)):
+        matrix[result[i][0]][result[i][1]] = replace_list[i]
+    replace_list = replace_list[len(result):]
 
     print(result)
