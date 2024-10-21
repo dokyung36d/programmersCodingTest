1755b1d67df319d64bc9f57754275938cb6898c1
dokyu <dokyung36@naver.com>
2024-10-21 22:30:09 +0900

    apply damage after all move finsih

--- 왕실의 기사 대결.py
+++ 왕실의 기사 대결.py
@@ -35,7 +35,7 @@ fighter_outed =[0] * N
 def check_move_available(edge, height, width):
     global trap_list
     ##맵 밖의 벽에 막힌 경우
-    if edge[0] < 0 or edge[0] + height > L or edge[1] < 0 or edge[1] + width > L:
+    if edge[0] < 0 or edge[0] + height - 1 > L - 1 or edge[1] < 0 or edge[1] + width - 1 > L - 1:
         return False
     
     for i in range(len(wall_list)):
@@ -53,7 +53,7 @@ def check_move_available(edge, height, width):
     return True
 
 
-def change_fighter_position(fighter_index, edge, direction_index, attack):
+def change_fighter_position(fighter_index, edge, direction_index, attack, damage_in_turn, moved):
     global map_matrix, total_damage
 
     height, width = fighter_info_list[fighter_index][2], fighter_info_list[fighter_index][3]
@@ -73,31 +73,58 @@ def change_fighter_position(fighter_index, edge, direction_index, attack):
     if not counter_fighter_list: ##이동한 자리애 상대방 기사가 없는 경우
         damage = get_damage(moved_edge, height, width)
 
-        if attack: ##자신이 공격한 경우
-            fighter_info_list[fighter_index] = (moved_edge[0], moved_edge[1], height, width, fighter_info_list[fighter_index][-1])
+        # if attack: ##자신이 공격한 경우
+        #     fighter_info_list[fighter_index] = (moved_edge[0], moved_edge[1], height, width, fighter_info_list[fighter_index][-1])
 
-            return True
-        total_damage += damage
-        damage_list[fighter_index] += damage
-        remain_health = fighter_info_list[fighter_index][-1] - damage
+        #     return True
+        
+        damage_in_turn[fighter_index] += damage
+        moved[fighter_index] = 1
+        
+        # total_damage += damage
+        # damage_list[fighter_index] += damage
+        # remain_health = fighter_info_list[fighter_index][-1] - damage
 
-        fighter_info_list[fighter_index] = (moved_edge[0], moved_edge[1], height, width, remain_health)
+        # fighter_info_list[fighter_index] = (moved_edge[0], moved_edge[1], height, width, remain_health)
 
-        if remain_health <= 0:
-            fighter_outed[fighter_index] = 1
+        # if remain_health <= 0:
+        #     fighter_outed[fighter_index] = 1
 
-        return True
+        # return True
     
 
     # result = change_fighter_position(counter_fighter_index, moved_edge, height = height, width = width)
     for counter_fighter_index in counter_fighter_list:
         counter_fighter_edge = (fighter_info_list[counter_fighter_index][0], fighter_info_list[counter_fighter_index][1])
-        result = change_fighter_position(counter_fighter_index, counter_fighter_edge, direction_index, False)
+        result = change_fighter_position(counter_fighter_index, counter_fighter_edge, direction_index, False, damage_in_turn, moved)
+        moved[counter_fighter_index] = 1
 
         if result == False:
             return False
+
+
+    # fighter_info_list[fighter_index] = (moved_edge[0], moved_edge[1], height, width, fighter_info_list[fighter_index][-1])
+
+    if not attack:
+        return True
     
-    fighter_info_list[fighter_index] = (moved_edge[0], moved_edge[1], height, width, fighter_info_list[fighter_index][-1])
+    # moved[fighter_index] = 1
+    
+    for i in range(N):
+        if moved[i] == 0:
+            continue
+        if i == fighter_index:
+            fighter_info_list[i] = (fighter_info_list[i][0] + direction[0], fighter_info_list[i][1] + direction[1], fighter_info_list[i][2], fighter_info_list[i][3], fighter_info_list[i][4])
+            continue
+
+        damage_list[i] += damage_in_turn[i]
+        remain_health = fighter_info_list[i][-1] - damage_in_turn[i]
+
+        if remain_health <= 0:
+            fighter_outed[i] = 1
+
+        fighter_info_list[i] = (fighter_info_list[i][0] + direction[0], fighter_info_list[i][1] + direction[1], fighter_info_list[i][2], fighter_info_list[i][3], remain_health)
+
     return True
     
 
@@ -105,7 +132,8 @@ def change_fighter_position(fighter_index, edge, direction_index, attack):
 
 def check_fight_avail(moved_edge, height, width, fighter_index): ##이동한 위치에 기존 기사가 존재하는 지 check
     fighter_avail_list = []
-    fighter = (moved_edge, fighter_info_list[fighter_index][2], fighter_info_list[fighter_index][3])
+    # fighter = (moved_edge, fighter_info_list[fighter_index][2], fighter_info_list[fighter_index][3])
+    fighter = (moved_edge, height, width)
 
     for i in range(len(fighter_info_list)):
         if fighter_outed[i] == 1 or fighter_index == i: ##이미 아웃된 경우, 자기 자신인 경우
@@ -180,7 +208,7 @@ for _ in range(Q):
 
     edge = (fighter_info_list[fighter_index][0], fighter_info_list[fighter_index][1])
  
-    result = change_fighter_position(fighter_index, edge, direction_index, True)
+    result = change_fighter_position(fighter_index, edge, direction_index, True, [0] * N, [0] * N)
 
 
 total_damage = 0
