c4370733e48dc065c5745c8ee08246837be5cdf6
dokyu <dokyung36@naver.com>
2024-10-28 15:53:10 +0900

    need to implement sum function first

--- 코드트리 DB.py
+++ 코드트리 DB.py
@@ -1,21 +1,33 @@
+## https://www.codetree.ai/training-field/frequent-problems/problems/codetree-db/description?page=1&pageSize=5
+
+## 출력하는 결과는 오직 rank와 sum
+## insert, delete는 순서대로 저장
+## 해당되는 범위면 고려
+
 import bisect
+from collections import defaultdict
 
 Q = int(input())
 
 main_list = []
 insert_list = []
 delete_list = []
-tree_list = []
+tree_list = [0] * 100000
 
 def init():
     return []
 
-def insert(menu, price):
+def insert(menu, price, order):
     global main_list
 
-    bisect.insort_left(main_list, (price, menu))
-    bisect.insort_left(insert_list, (price, menu))
+    bisect.insort_left(main_list, (price, menu, order))
+    # bisect.insort_left(insert_list, (price, menu, order))
 
+
+##삭제한 name이 이후에 추가로 insert 될 수 있음
+##모든 name에 대해서 탐색해야 하므로 무조건 O(n) -> 모든 node에 대해 delete가 전달이 되어야 함(-> 진행 시점도 저장해야 함)
+## -> Late Propagation 진행
+##delete가 올 때마다 탐색하는 각 노드는 delete 여부 확인, 확인하면 대기 list에서 삭제
 def delete(name, order):
     global delete_list
 
@@ -40,12 +52,12 @@ def make_tree():
     pass
 
 ##sum에서 값을 구할 때 사용했던 값들을 나중에도 사용해야함
-def sum(search_list, value, start = True):
+def sum(search_list, value, depth, tree_index):
     global tree_list
 
     total = 0
 
-    if start == True: ##미뤄났던 delete를 반영
+    if depth == 0: ##미뤄났던 delete를 반영
         index = bisect.insort_left(value)
         apply_deleted((0, index))
 
@@ -59,10 +71,21 @@ def sum(search_list, value, start = True):
         
     if not tree_list:
         make_tree()
-        
+
     left_list = search_list[:len(search_list) // 2]
     right_list = search_list[len(search_list) // 2:]
+    
+    mid_value = search_list[len(search_list) // 2]
 
+    if mid_value < value: ##중간값이 원하는 값보다 작을 때
+        total += sum(left_list, value, depth + 1, 2 * tree_index + 1)
+    
+    else:
+        left_sum = sum(left_list, value, depth + 1, 2 * tree_index + 1)
+        tree_list[2 * tree_index + 1] = left_sum
+
+        total += left_sum
+        total += sum(right_list, value, depth + 1, 2 * tree_index + 2)
 
 
 
