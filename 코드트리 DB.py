fa666b1e7e73f386e50e55454b1431efe384651d
dokyu <dokyung36@naver.com>
2024-10-30 11:16:13 +0900

    first TC passed, but second case failed

--- 코드트리 DB.py
+++ 코드트리 DB.py
@@ -11,7 +11,7 @@ Q = int(input())
 
 tree_dict = defaultdict(list)
 
-tree_dict[0] = [0, (0, 10 ** 9), defaultdict(int), defaultdict(str)]
+tree_dict[0] = [0, (0, 10 ** 9 + 1), defaultdict(int), defaultdict(str)]
 
 
 ##(갈라짐의 기준, sum, 자신 아래에서의 name dict)
@@ -49,6 +49,48 @@ def init():
 
 #     return return_list
 
+def apply_node(index, gap, menu, price, insert=True):
+    global tree_dict
+
+    if len(tree_dict[index]) == 0:
+        local_menu_price_dict = defaultdict(int)
+        local_price_menu_dict = defaultdict(str)
+
+
+        if insert:
+            local_menu_price_dict[menu] = price
+            local_price_menu_dict[price] = menu
+
+            tree_dict[index] = (price, gap,
+                                local_menu_price_dict, local_price_menu_dict, 
+                                1)
+            
+            return
+        
+        tree_dict[index] = (0, gap,
+                            local_menu_price_dict, local_price_menu_dict, 
+                            0)
+        return
+    
+    
+    local_menu_price_dict, local_price_menu_dict = tree_dict[index][2], tree_dict[index][3]
+
+    local_menu_price_dict[menu] = price
+    local_price_menu_dict[price] = menu
+
+    tree_dict[index] = (tree_dict[index][0] + price, tree_dict[index][1],
+                            local_menu_price_dict, local_price_menu_dict, 
+                            tree_dict[index][-1] + 1)
+    
+    return
+
+def check_node_exist(index):
+    global tree_dict
+
+    if len(tree_dict[index]) == 0:
+        return False
+    
+    return True
 
 def insert(menu, price):
     global tree_dict
@@ -61,20 +103,13 @@ def insert(menu, price):
     if tree_dict[0][3][price] != "":
         return 0
 
+    apply_node(index, (0, 10 ** 9 + 1), menu, price)
+
 
     while True:
         # if len(tree_dict[index]) == 0:
         #     pass
-
-        local_menu_price_dict, local_price_menu_dict = tree_dict[index][2], tree_dict[index][3]
-
-        local_menu_price_dict[menu] = price
-        local_price_menu_dict[price] = menu
-
-        tree_dict[index] = (tree_dict[index][0] + price, local_menu_price_dict, local_price_menu_dict, tree_dict[index][-1] + 1)
-
-
-        start, end = tree_dict[index][1][1] - tree_dict[index][1][0]
+        start, end = tree_dict[index][1][0], tree_dict[index][1][1]
         mid_value = (start + end) // 2
 
         gap = end - start
@@ -87,9 +122,11 @@ def insert(menu, price):
 
         if left_gap[0] <= price < left_gap[1]:
             index = 2 * index + 1
+            apply_node(index, left_gap, menu, price)
 
         elif right_gap[0] <= price < right_gap[1]:
             index = 2 * index + 2
+            apply_node(index, right_gap, menu, price)
 
     return 1
 
@@ -149,6 +186,9 @@ def rank(k):
     index = 0
 
     while True:
+        if k == 1:
+            pass
+
         left_node_index = 2 * index + 1
         right_node_index = 2 * index + 2
 
@@ -159,8 +199,13 @@ def rank(k):
 
         if gap == 1:
             return index + 1
+        
 
+        ## 왼쪽 노드가 비어있는 경우 error 발생
         if k <= left_node[-1]:
+            if k == 1:
+                name = list(tree_dict[index][2].keys())[0]
+                return name
             index = 2 * index + 1
 
         elif k > left_node[-1]:
@@ -170,14 +215,14 @@ def rank(k):
 
 ## 갈라질 것의 부모의 index를 return
 ## insert에서 들어오는 value는 전부 다름
-def get_tree_node_index(value):
-    global tree_list
+# def get_tree_node_index(value):
+#     global tree_list
 
-    path = find_path(value)
+#     path = find_path(value)
     
-    node = tree_list[path[-1]]
+#     node = tree_list[path[-1]]
 
-    return node[1]
+#     return node[1]
 
 ##sum에서 값을 구할 때 사용했던 값들을 나중에도 사용해야함
 def sum(value):
@@ -194,17 +239,25 @@ def sum(value):
         left_node_index = 2 * index + 1
         right_node_index = 2 * index + 2
 
+
+        if len(tree_dict[left_node_index]) == 0:
+            apply_node(left_node_index, (start, (start + end) // 2),
+                        0, 0, insert=False)
+
+        if len(tree_dict[right_node_index]) == 0:
+            apply_node(right_node_index, ((start + end) // 2, end),
+                        0, 0, insert=False)
+
         left_node = tree_dict[left_node_index]
         right_node = tree_dict[right_node_index]
-
         if gap == 1:
             break
 
         mid_value = (start + end) // 2
 
         if value > mid_value:
-            total_sum += left_node[0]
             index = 2 * index + 2
+            total_sum += left_node[0]
 
         elif value <= mid_value:
             index = 2 * index + 1
@@ -212,25 +265,25 @@ def sum(value):
     return total_sum
 
 
-# for i in range(Q):
-#     command = command_list[i]
-#     print(command)
+for i in range(Q):
+    command = list(input().split())
+    print(command)
 
-#     if command[0] == "init":
-#         init()
+    if command[0] == "init":
+        init()
 
-#     elif command[0] == "insert":
-#         result = insert(command[1], int(command[2]))
-#         print(result)
+    elif command[0] == "insert":
+        result = insert(command[1], int(command[2]))
+        print(result)
 
-#     elif command[0] == "delete":
-#         result = delete(command[1])
-#         print(result)
+    elif command[0] == "delete":
+        result = delete(command[1])
+        print(result)
 
-#     elif command[0] == "rank":
-#         result = rank(int(command[1]))
-#         print(result)
+    elif command[0] == "rank":
+        result = rank(int(command[1]))
+        print(result)
 
-#     elif command[0] == "sum":
-#         result = sum(int(command[1]))
-#         print(result)
+    elif command[0] == "sum":
+        result = sum(int(command[1]))
+        print(result)
