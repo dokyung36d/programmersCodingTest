bbdf29b4ef995dfae3c0c16e8ec382f0a02c5c2d
dokyu <dokyung36@naver.com>
2024-11-14 15:25:35 +0900

    works well, but too many time

--- 2806. N-Queen.py
+++ 2806. N-Queen.py
@@ -1,14 +1,19 @@
-n = int(input())
+##https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AV7GKs06AU0DFAXB&categoryId=AV7GKs06AU0DFAXB&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=ALL&select-1=3&pageSize=10&pageIndex=1
+import copy
+import bisect
+from collections import defaultdict
+from os import remove
 
-matrix = [[0 for _ in range(n)] for _ in range(n)]
-print(matrix)
 
 def queen(indices):
+    global n
     removedIndices = []
 
     row, col = indices[0], indices[1]
 
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (1, 1), (-1, -1), (-1, 1), (1, -1)]
+
+    removedIndices.append(indices)
 
     for direction in directions:
         mutiply = 1
@@ -20,29 +25,69 @@ def queen(indices):
 
             if not checkIndex((rowChanged, colChanged)):
                 break
-            removedIndices.append((rowChanged, colChanged))
+
+            index = bisect.bisect_left(removedIndices, (rowChanged, colChanged))
+            removedIndices.insert(index, (rowChanged, colChanged))
 
             mutiply += 1
 
     return removedIndices
 
-    # for i in range(n):
-    #     removedRowIndices = (row, i)
-    #     removedColIndices = (i, col)
-    #
-    #     removedIndices.append(removedRowIndices)
-    #     removedIndices.append(removedColIndices)
-    #
-    # if row >= col:
-    #     leftDiagonal = (row - col, 0)
-    # else:
-    #     leftDiagonal = (0, col - row)
-    #
-    # if row + col <= n:
-    #     rightDiagonal = (row + col, 0)
-    # else:
-    #     rightDiagonal = (row + col + 1 - n, n - 1)
 
+##Just For Visualization
+def applyToMap(mapInfo, removedIndices):
+    global matrix
+
+    for removedIndex in removedIndices:
+        matrix[removedIndex[0]][removedIndex[1]] = 1
+
+def applyToDict(mapDict, removedIndices):
+    mapDict = copy.deepcopy(mapDict)
+    for removedIndex in removedIndices:
+        mapDict[removedIndex] = 1
+
+    return mapDict
+
+def applyToSortedList(sortedList, removedIndices):
+    sortedList = sortedList[:]
+
+    if len(sortedList) == 0:
+        sortedList.append(removedIndices[0])
+
+    for removedIndex in removedIndices:
+        index = bisect.bisect_left(sortedList, removedIndex)
+
+        if sortedList[index] == removedIndex:
+            continue
+
+        sortedList.insert(index, removedIndex)
+
+    return sortedList
+
+
+
+def setQueen(startIndices, numRemainQueen, queenIndices):
+    global n, matrix, cnt
+
+    for i in range(startIndices[0], n):
+        for j in range(startIndices[1], n):
+            removedIndices = queen((i, j))
+
+
+            ## if already chose
+            if not checkCompatibleWithAnotherQueen(removedIndices, queenIndices):
+                continue
+
+            if numRemainQueen == 1:
+                cnt += 1
+                continue
+
+            nextIndices = getNextIndex((i, j))
+            if nextIndices == False:
+                return
+
+
+            setQueen(nextIndices, numRemainQueen - 1, queenIndices + [(i, j)])
 
 
 def checkIndex(indices):
@@ -50,3 +95,58 @@ def checkIndex(indices):
         return False
 
     return True
+
+def existInSortedList(sortedList, value):
+    index = bisect.bisect_left(sortedList, value)
+    if len(sortedList) == 0:
+        return False
+
+    if sortedList[index] == value:
+        return True
+
+    return False
+
+def getNextIndex(indices):
+    global n
+    row, col = indices[0], indices[1]
+
+    if row == n - 1 and col == n - 1:
+        return False
+
+    return (row + 1, 0)
+
+    # if col == n - 1:
+    #     return (row + 1, 0)
+    #
+    # return (row, col + 1)
+
+def checkCompatibleWithAnotherQueen(removedIndices, queenIndices):
+    for queenIndex in queenIndices:
+        index = bisect.bisect_left(removedIndices, queenIndex)
+
+        if removedIndices[index] == queenIndex:
+            return False
+
+    return True
+
+cnt = 0
+n = int(input())
+setQueen((0, 0), n, [])
+print(cnt)
+
+# T = int(input())
+#
+# for _ in range(1, T + 1):
+#     n = int(input())
+#     cnt = 0
+#     setQueen((0, 0), n, defaultdict(int))
+#
+#     print(f"#{_} {cnt}")
+#
+# removedIndices = queen((2, 1))
+#
+# for removedIndex in removedIndices:
+#     matrix[removedIndex[0]][removedIndex[1]] = 1
+#
+# for row in matrix:
+#     print(row)
