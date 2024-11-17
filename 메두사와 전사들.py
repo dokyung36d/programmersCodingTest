731fc8100495991ec29c32b820a13c4c697fd720
dokyu <dokyung36@naver.com>
2024-11-17 23:03:05 +0900

    need to implement medusa

---
+++ 메두사와 전사들.py
@@ -0,0 +1,12 @@
+##SWEA 체스 문제랑 유사하게 풀기
+
+def calculateDistance(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+def checkBelong(pos, startPos, mainDirection, sideDirection):
+    deltaRow, deltCol = pos[0] - startPos[0], pos[1] - startPos[1]
+
+    straightPos = (startPos)
+
+def decomposeVector():
+    pass
