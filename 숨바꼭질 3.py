99b56db214ba0bb460ac1bf54dd876b2993e9fce
dokyu <dokyung36@naver.com>
2024-12-19 21:56:16 +0900

    hide and seek 3 solved

--- 숨바꼭질 3.py
+++ 숨바꼭질 3.py
@@ -6,36 +6,37 @@ import heapq
 
 def solution():
     N, K = map(int, sys.stdin.readline().split())
+    if N >= K:
+        return N - K
     queue = []
     heapq.heappush(queue, (0, N))
-    dpDict = defaultdict(int)
+    visitedDict = defaultdict(int)
 
-    dpDict[N] = 1
+    visitedDict[N] = 1
 
     while queue:
         depth, value = heapq.heappop(queue)
         # print(depth, value)
         
+        visitedDict[value] = 1
+
         if value == K:
             return depth
 
-        assert dpDict[value] != 0
 
-
-        if dpDict[value - 1] == 0 and value >= 1:
-            dpDict[value - 1] = dpDict[value] + 1
+        if visitedDict[value - 1] == 0 and value >= 1:
             heapq.heappush(queue, (depth + 1, value - 1))
 
-        if dpDict[value + 1] == 0:
-            dpDict[value + 1] = dpDict[value] + 1
+        if visitedDict[value + 1] == 0:
             heapq.heappush(queue, (depth + 1, value + 1))
+            
 
         while value <= K and value != 0:
             # print(value)
-            if dpDict[2 * value] == 0:
-                dpDict[2 * value] = dpDict[value]
+            if visitedDict[2 * value] == 0:
+                visitedDict[2 * value] = visitedDict[value]
                 heapq.heappush(queue, (depth, 2 * value))
-            
+
             value = 2 * value
 
 
